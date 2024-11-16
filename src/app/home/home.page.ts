5baec1f93e06baee858891d124e4c1699861ae85
Kenzeev Andrei <kenzeev04@list.ru>
2024-11-16 18:30:21 +0300

    ad

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+ s="12"
+ b="13"
+ letter = ["s","b"]
+
+ books=[
+  {a:"Пушка",n:"Гец"},
+  {a:"Камень",n:"Лужа"}]
+
   constructor() {}
 
 }
